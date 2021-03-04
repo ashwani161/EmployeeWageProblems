854e3d5f23d67cf6c2d23875d38406fedff14406
ashwani161 <ashwanin65@gmail.com>
2021-03-04 11:33:51 +0530

    Added Daily employeeWage

--- EmployeeWagecomputation.sh
+++ EmployeeWagecomputation.sh
@@ -11,3 +11,12 @@ then
 else
      echo "Employee is absent";
 fi
+
+if [ $isPresent -eq $randomCheck ];
+then
+     empRatePerHr=20;
+     empHrs=8;
+     salary=$(($empHrs*$empRatePerHr));
+else
+     salary=0;
+fi
