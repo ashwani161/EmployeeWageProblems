5833eeb660a48c4c54879afe814d21c3abd59272
ashwani161 <ashwanin65@gmail.com>
2021-03-04 11:16:41 +0530

    Added Employeeattendance

--- EmployeeWagecomputation.sh
+++ EmployeeWagecomputation.sh
@@ -2,3 +2,12 @@
 echo "Welcome to Employee";
 echo "Wage Computation";
 echo "Program on Master Branch";
+
+isPresent=1;
+randomCheck=$((RANDOM%2));
+if [ $isPresent -eq $randomCheck ];
+then
+     echo "Employee is present";
+else
+     echo "Employee is absent";
+fi
