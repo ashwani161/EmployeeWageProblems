623c8d3e7bd96c9458e3c3a48c8181bcb18224e9
ashwani161 <ashwanin65@gmail.com>
2021-03-04 17:13:32 +0530

    Added refactor the code to get working hours

--- EmployeeWagecomputation.sh
+++ EmployeeWagecomputation.sh
@@ -96,3 +96,36 @@ do
       totalEmpHr=$(($totalEmpHr+$empHrs))
 done
 totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR));
+
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=4;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
+
+totalEmpHr=0;
+totalWorkingDays=0;
+
+function getWorkingHours() {
+                case $1 in
+                         $IS_FULL_TIME)
+                             workingHours=8
+                             ;;
+                         $IS_PART_TIME)
+                             workingHours=4
+                             ;;
+                          *)
+                             workingHours=0
+                             ;;
+                           esac
+                           echo $workingHours
+}
+while [[ $totalWorkingHours -lt $MAX_HRS_IN_MONTH &&
+                         $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+         ((totalWorkingdays++))
+         workingHours="$( getWorkingHours $((RANDOM%3)) )"
+         totalWorkingHours=$(($totalWorkingHOurs+$workingHours))
+done
+
+totalSalary=$(($totalWorkingHours*$EMP_RATE_PER_HR));
